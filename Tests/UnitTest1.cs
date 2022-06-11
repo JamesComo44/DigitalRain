cb9742286967579f3159303fdcb5a75771e88ae6
Ethan Hearne <ehearne3@gatech.edu>
2022-06-11 14:31:17 -0400

    Move code to inner folder.  Add tests folder next to it.

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DigitalRain;
+
+namespace DigitalRainTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var game = new DigitalRainGame();
+            System.Console.Out.WriteLine("Hello for the last time?");
+        }
+    }
+}
