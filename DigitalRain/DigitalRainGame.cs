800dcc02cefe7144090451463ce08d9b50a56c59
James Como <jamescomo44@gmail.com>
2022-06-30 22:50:16 -0400

    Moved all logic from Game class to dedicated class

--- DigitalRain/DigitalRainGame.cs
+++ DigitalRain/DigitalRainGame.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -13,14 +12,12 @@ namespace DigitalRain
     public class DigitalRainGame : Game
     {
         private GraphicsDeviceManager _graphics;
-        private int _screenWidth;
-        private int _screenHeight;
+        private Rectangle _screenBounds;
         private SpriteBatch _spriteBatch;
         private SpriteFont _font;
         private float _fontHeight;
-        private UnoccupiedColumnPool _columnPool;
-        private RaindropStreamFactory _streamFactory;
-        private RaindropStreams _raindropStreams;
+
+        private StreamSpawner _spawner;
 
         public DigitalRainGame()
         {
@@ -31,15 +28,8 @@ namespace DigitalRain
 
         protected override void Initialize()
         {
-            Rectangle bounds = _graphics.GraphicsDevice.Viewport.Bounds;
-            _screenWidth = bounds.Width;
-            _screenHeight = bounds.Height;
-
-            var columnNumberPicker = new RandomColumnNumberPicker(columnCount: 50);
-            _columnPool = new UnoccupiedColumnPool(columnNumberPicker, _graphics.GraphicsDevice.Viewport.Bounds);
-
-            _streamFactory = new RaindropStreamFactory(_columnPool);
-            _raindropStreams = new RaindropStreams();
+            _screenBounds = _graphics.GraphicsDevice.Viewport.Bounds;
+            _spawner = new StreamSpawner(_screenBounds);
 
             base.Initialize();
         }
@@ -56,40 +46,18 @@ namespace DigitalRain
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            AddNewRaindropStreams(gameTime);
-            RemoveDeadRaindropStreams();
-            _raindropStreams.Update(gameTime);
+            _spawner.Update(gameTime, _fontHeight);
 
             base.Update(gameTime);
         }
 
-        private void AddNewRaindropStreams(GameTime gameTime)
-        {
-            float newRaindropStreamsPerSecond = 3;
-            float secondsPerNewRaindropStream = 1 / newRaindropStreamsPerSecond;
-            var numRaindropStreams = (int)(gameTime.TotalGameTime.TotalSeconds / secondsPerNewRaindropStream);
-            while (!_columnPool.IsEmpty && _raindropStreams.Count < numRaindropStreams)
-            {
-                var raindropStream = _streamFactory.Create(_fontHeight);
-                _raindropStreams.Add(raindropStream);
-            }
-        }
-
-        private void RemoveDeadRaindropStreams()
-        {
-            var deadStreams = _raindropStreams.RemoveDeadStreams();
-            var columnsToRestore = from stream in deadStreams select stream.Column;
-            _columnPool.Restore(new HashSet<Column>(columnsToRestore));
-        }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
 
             _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.PointClamp);
-
-            _raindropStreams.Draw(gameTime, _spriteBatch, _font);
-
+            _spawner.Draw(gameTime, _spriteBatch, _font);
             _spriteBatch.End();
 
             base.Draw(gameTime);
@@ -101,41 +69,41 @@ namespace DigitalRain
         {
             string text = "Hello World S A 10 893354 I l L I";
             Vector2 textMiddlePoint = _font.MeasureString(text) / 2;
-            Vector2 position = new Vector2(_screenWidth / 2f, _screenHeight / 2.3f);
+            Vector2 position = new Vector2(_screenBounds.X / 2f, _screenBounds.Y / 2.3f);
 
             _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.PointClamp);
             string draw_text = text + " PointClamp";
-            position = new Vector2(_screenWidth / 2.5f, _screenHeight / 2.1f);
+            position = new Vector2(_screenBounds.X / 2.5f, _screenBounds.Y / 2.1f);
             _spriteBatch.DrawString(_font, draw_text, position, Color.GreenYellow, 0, textMiddlePoint, 1.0f, SpriteEffects.None, 0.5f);
             _spriteBatch.End();
 
             _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.AnisotropicClamp);
             draw_text = text + " AnisotropicClamp";
-            position = new Vector2(_screenWidth / 2.5f, _screenHeight / 2.6f);
+            position = new Vector2(_screenBounds.X / 2.5f, _screenBounds.Y / 2.6f);
             _spriteBatch.DrawString(_font, draw_text, position, Color.GreenYellow, 0, textMiddlePoint, 1.0f, SpriteEffects.None, 0.5f);
             _spriteBatch.End();
 
             _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.LinearClamp);
             draw_text = text + " LinearClamp";
-            position = new Vector2(_screenWidth / 2.5f, _screenHeight / 3f);
+            position = new Vector2(_screenBounds.X / 2.5f, _screenBounds.Y / 3f);
             _spriteBatch.DrawString(_font, draw_text, position, Color.GreenYellow, 0, textMiddlePoint, 1.0f, SpriteEffects.None, 0.5f);
             _spriteBatch.End();
 
             _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.PointWrap);
             draw_text = text + " PointWrap";
-            position = new Vector2(_screenWidth / 2.5f, _screenHeight / 3.5f);
+            position = new Vector2(_screenBounds.X / 2.5f, _screenBounds.Y / 3.5f);
             _spriteBatch.DrawString(_font, draw_text, position, Color.GreenYellow, 0, textMiddlePoint, 1.0f, SpriteEffects.None, 0.5f);
             _spriteBatch.End();
 
             _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.LinearWrap);
             draw_text = text + " LinearWrap";
-            position = new Vector2(_screenWidth / 2.5f, _screenHeight / 4.5f);
+            position = new Vector2(_screenBounds.X / 2.5f, _screenBounds.Y / 4.5f);
             _spriteBatch.DrawString(_font, draw_text, position, Color.GreenYellow, 0, textMiddlePoint, 1.0f, SpriteEffects.None, 0.5f);
             _spriteBatch.End();
 
             _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.AnisotropicWrap);
             draw_text = text + " AnisotropicWrap";
-            position = new Vector2(_screenWidth / 2.5f, _screenHeight / 5.8f);
+            position = new Vector2(_screenBounds.X / 2.5f, _screenBounds.Y / 5.8f);
             _spriteBatch.DrawString(_font, draw_text, position, Color.GreenYellow, 0, textMiddlePoint, 1.0f, SpriteEffects.None, 0.5f);
             _spriteBatch.End();
         }
---
+++ DigitalRain/Raindrops/StreamSpawner.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DigitalRain.Raindrops
+{
+    using Columns;
+    using Raindrops;
+
+    class StreamSpawner
+    {
+        private UnoccupiedColumnPool _columnPool;
+        private RaindropStreamFactory _streamFactory;
+        private RaindropStreams _raindropStreams;
+
+        public StreamSpawner(Rectangle screenBounds)
+        {
+            var columnNumberPicker = new RandomColumnNumberPicker(columnCount: 50);
+            _columnPool = new UnoccupiedColumnPool(columnNumberPicker, screenBounds);
+
+            _streamFactory = new RaindropStreamFactory(_columnPool);
+            _raindropStreams = new RaindropStreams();
+        }
+
+        public void Update(GameTime gameTime, float currentFontHeight)
+        {
+            AddNewRaindropStreams(gameTime, currentFontHeight);
+            RemoveDeadRaindropStreams();
+            _raindropStreams.Update(gameTime);
+        }
+
+
+        private void AddNewRaindropStreams(GameTime gameTime, float currentFontHeight)
+        {
+            float newRaindropStreamsPerSecond = 3;
+            float secondsPerNewRaindropStream = 1 / newRaindropStreamsPerSecond;
+            var numRaindropStreams = (int)(gameTime.TotalGameTime.TotalSeconds / secondsPerNewRaindropStream);
+            while (!_columnPool.IsEmpty && _raindropStreams.Count < numRaindropStreams)
+            {
+                var raindropStream = _streamFactory.Create(currentFontHeight);
+                _raindropStreams.Add(raindropStream);
+            }
+        }
+
+        private void RemoveDeadRaindropStreams()
+        {
+            var deadStreams = _raindropStreams.RemoveDeadStreams();
+            var columnsToRestore = from stream in deadStreams select stream.Column;
+            _columnPool.Restore(new HashSet<Column>(columnsToRestore));
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch, SpriteFont font)
+        {
+            _raindropStreams.Draw(gameTime, spriteBatch, font);
+        }
+    }
+}
